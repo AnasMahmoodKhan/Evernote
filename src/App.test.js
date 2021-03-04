ba86a6f46f6f6bf6c4d096b567a8190e1206f4e4
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-04 17:48:02 +0530

    Unit Tests #1 Added

--- src/App.test.js
+++ src/App.test.js
@@ -1,41 +1,44 @@
 // import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { configure, shallow } from "enzyme";
+import { configure, shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import Navbar from "./components/layout/Navbar";
 
-// import { createStore, applyMiddleware, compose } from "redux";
-// import rootReducer from "./store/reducers/rootReducer";
-// import { Provider } from "react-redux";
-// import firebase from "firebase/app";
-// import thunk from "redux-thunk";
-// import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
-// import {
-//   createFirestoreInstance,
-//   getFirestore,
-//   reduxFirestore,
-// } from "redux-firestore";
-// import firebaseConfig from "./config/fbconfig";
+import Navbar from "./components/layout/Navbar";
+import Form from "./components/home/Form";
+
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import firebase from "firebase/app";
+import thunk from "redux-thunk";
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import {
+  createFirestoreInstance,
+  getFirestore,
+  reduxFirestore,
+} from "redux-firestore";
+import firebaseConfig from "./config/fbconfig";
+import NoteList from "./components/notes/NoteList";
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebaseConfig)
+  )
+);
+
+const rrfProps = {
+  firebase,
+  config: firebaseConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
 
 configure({ adapter: new Adapter() });
 
-// const store = createStore(
-//   rootReducer,
-//   compose(
-//     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-//     reduxFirestore(firebaseConfig)
-//   )
-// );
-
-// const rrfProps = {
-//   firebase,
-//   config: firebaseConfig,
-//   dispatch: store.dispatch,
-//   createFirestoreInstance,
-// };
-
 describe("Testing Evernote", () => {
-  test("renders learn react link", () => {
+  it("renders Route link in App", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("Route")).toBeTruthy();
     // render(
@@ -49,10 +52,50 @@ describe("Testing Evernote", () => {
     // expect(linkElement).toBeInTheDocument();
   });
 
-  test("renders a link with text of NoteBook", () => {
+  it("renders a link with text of NoteBook", () => {
     const wrapper = shallow(<Navbar />);
     const text = wrapper.find("Link").text();
     expect(text).toBe("NoteBook");
+  });
+
+  it("renders props of Form", () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <Form />
+        </ReactReduxFirebaseProvider>
+      </Provider>
+    );
+
+    expect(wrapper.props()).toBeDefined();
+  });
+
+  it("renders props of NoteList", () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <NoteList />
+        </ReactReduxFirebaseProvider>
+      </Provider>
+    );
+
+    expect(wrapper.props()).toBeDefined();
+  });
+
+  it("calls onSubmit prop function when form is submitted", () => {
+    const handleSubmit = jest
+      .fn()
+      
+    const wrapper = mount(
+      <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <Form  handleSubmit={handleSubmit} />
+        </ReactReduxFirebaseProvider>
+      </Provider>
+    );
+
+    wrapper.find("form").simulate("submit");
+    expect(handleSubmit).toBeCalledTimes(1);
   });
 
   // test("renders new note link", () => {
